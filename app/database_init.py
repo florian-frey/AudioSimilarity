7649c5253271aaf005824074e643252930a27f6e
florian-frey <83298480+florian-frey@users.noreply.github.com>
2023-07-21 00:40:41 +0200

    Update database_init.py
    change postgres to redis

--- app/database_init.py
+++ app/database_init.py
@@ -1,97 +1,74 @@
 import pandas as pd
-import psycopg2
-import zipfile
-from time import sleep
+import numpy as np
 from tqdm import tqdm
-import toml
+from redis import Redis
+from redis.commands.search.indexDefinition import IndexDefinition, IndexType
+from redis.commands.search.field import VectorField, TextField, NumericField
 
 
-secrets = toml.load("/code/.streamlit/secrets.toml")["connections"]["postgres"]
+index_name = "audiosimilarity"
+distance_metric:str="COSINE"
+DIM = 100
 
-for retry in range(1,6):
-    print("Connecting to database...")
-    try:
-        conn = psycopg2.connect(f"host={secrets['host']} port={secrets['port']} dbname={secrets['database']}  user={secrets['username']}  password={secrets['password']}")
-        cursor = conn.cursor()
-        break
-    except:
-        print(f"Retrying in {5*retry} seconds...")
-        sleep(5*retry)
+# load data
+df = pd.read_csv("../fma-metadata/tracks_small.csv", index_col=0, header=[0,1])
 
-cursor.execute("SELECT EXISTS (SELECT FROM pg_tables WHERE schemaname = 'public' AND tablename  = 'tracks');")
-if cursor.fetchone()[0]:
-    print("Table 'tracks' already exists.")
-    cursor.close()
-    conn.close()
-
-else:
-    print("Extracting Files...")
-
-    # read and preprocess metadata
-    with zipfile.ZipFile("tracks.zip", 'r') as zip_ref:
-        zip_ref.extractall("./")
-
-    data = pd.read_csv("tracks.csv", index_col=0, header=[0, 1])
-
-    data[("album", "date_created")] = pd.to_datetime(data[("album", "date_created")])
-    data[("album", "date_released")] = pd.to_datetime(data[("album", "date_released")])
-
-    data[("track", "date_created")] = pd.to_datetime(data[("track", "date_created")])
-    data[("track", "date_recorded")] = pd.to_datetime(data[("track", "date_recorded")])
-
-    data = data.dropna(subset=[("track", "title")])
+# redis connection
+redis_conn = Redis(host='localhost', port=6379, password=None)
 
 
-    # write data to db
-    print("Initializing database (may take a few minutes)...")
+# define fields
+track_title = TextField(name="track_title")
+album_title = TextField(name="album_title")
+artist_name = TextField(name="artist_name")
+track_publisher = TextField(name="track_publisher")
 
-    cursor.execute("""
-        CREATE TABLE tracks (
-            track_id            INT PRIMARY KEY,
-            track_title         VARCHAR NOT NULL,
-            track_bit_rate      INT,
-            track_date_recorded DATE,
-            track_duration      INT,
-            track_genre_top     VARCHAR,
-            track_genres        VARCHAR,
-            track_language_code VARCHAR,
-            track_tags          VARCHAR,
-            track_publisher     VARCHAR,
-            album_title         VARCHAR,
-            album_tracks        INT,
-            album_type          VARCHAR,
-            album_date_released DATE,
-            artist_name         VARCHAR,
-            artist_location     VARCHAR
-            )
-                """)
+album_tracks = NumericField(name="album_tracks")
+bit_rate = NumericField(name="bit_rate")
+duration = NumericField(name="duration")
+genre_top = TextField(name="genre_top")
 
-    for idx, row in tqdm(data.iterrows()):
-        row = row.replace({pd.NaT: None})
-        cursor.execute('''
-                    INSERT INTO tracks (track_id, track_title, track_bit_rate, track_date_recorded, track_duration, track_genre_top,
-                    track_genres, track_language_code, track_tags, track_publisher, album_title, album_tracks, album_type, album_date_released,
-                    artist_name, artist_location) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);
-                    ''',
-                    (
-                    idx,
-                    row["track", "title"],
-                    row["track", "bit_rate"],
-                    row["track", "date_recorded"],
-                    row["track", "duration"],
-                    row["track", "genre_top"],
-                    row["track", "genres"],
-                    row["track", "language_code"],
-                    row["track", "tags"],
-                    row["track", "publisher"],
-                    row["album", "title"],
-                    row["album", "tracks"],
-                    row["album", "type"],
-                    row["album", "date_released"],
-                    row["artist", "name"],
-                    row["artist", "location"]
-                    ))
+language_code = TextField(name="language_code")
+album_date_released = TextField(name="album_date_released")
 
-    conn.commit()
-    cursor.close()
-    conn.close()
+feature_vector = VectorField("feature_vector",
+            "HNSW", {
+                "TYPE": "FLOAT32",
+                "DIM": DIM,
+                "DISTANCE_METRIC": distance_metric,
+                "INITIAL_CAP": 10000,
+            })
+
+# create index
+redis_conn.ft(index_name).create_index(
+    fields = [track_title, album_title, artist_name, track_publisher, album_tracks, bit_rate, duration, genre_top, language_code, album_date_released, feature_vector],
+    definition = IndexDefinition(prefix=[index_name], index_type=IndexType.HASH)
+)
+
+print("Index created.")
+print("Populating database... May take a few minutes.")
+
+# populate database
+for track_id, row in tqdm(df.iloc[:,:].iterrows()):
+
+    row = row.replace({pd.NaT: "null"})
+
+    redis_conn.hset(
+        f"{index_name}:{track_id}",
+        mapping={
+            "track_title": row["track", "title"],
+            "album_title": row["album", "title"],
+            "artist_name": row["artist", "name"],
+            "track_publisher": row["track", "publisher"],
+            "album_tracks":  row["album", "tracks"],
+            "bit_rate": row["track", "bit_rate"],
+            "duration": row["track", "duration"],
+            "genre_top": row["track", "genre_top"],
+            "language_code": row["track", "language_code"],
+            "album_date_released": row["album", "date_released"],
+            "feature_vector": np.random.rand(DIM).astype(dtype=np.float32).tobytes()
+        }
+    )
+
+
+print("Database ready. Starting app...")
